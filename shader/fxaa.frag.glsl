67b096da8576c9fafd332658203be77f7c135cb4
Georg Lehmann <dadschoorse@gmail.com>
2019-11-01 12:42:15 +0100

    add fxaa shader

---
+++ shader/fxaa.frag.glsl
@@ -0,0 +1,29 @@
+#version 450
+#extension  GL_GOOGLE_include_directive : require
+
+#define FXAA_QUALITY_PRESET 39
+#define FXAA_GLSL_130 1
+#define FXAA_PC 1
+#define FXAA_GREEN_AS_LUMA 1
+#include "fxaa3_11.h"
+
+layout(set=0, binding=0) uniform sampler2D img;
+layout(set=1, binding=0) uniform CasBufferObject{
+    float sharpness;
+} cbo;
+
+layout(location = 0) in vec2 textureCoord;
+layout(location = 0) out vec4 fragColor;
+
+void main()
+{
+    vec2 size = vec2(textureSize(img,0));
+    vec2 fxaaQualityRcpFrame = vec2(1.0)/size;
+    float fxaaQualitySubpix = 0.75;
+    float fxaaQualityEdgeThreshold = 0.125;
+    float fxaaQualityEdgeThresholdMin = 0.0312; 
+    
+    vec4 zero = vec4(0.0);
+    
+    fragColor = FxaaPixelShader(textureCoord, zero, img, img, img, fxaaQualityRcpFrame, zero, zero, zero, fxaaQualitySubpix, fxaaQualityEdgeThreshold, fxaaQualityEdgeThresholdMin, 8.0, 0.125, 0.05, zero);
+}
