c6eb5f9b8706dfecd36a7d1e3f57631fd0043900
Georg Lehmann <dadschoorse@gmail.com>
2019-11-01 17:25:12 +0100

    Change Layout back to present the spec says that images are in present layout afer vkAcquireNextImageKHR

--- src/command_buffer.cpp
+++ src/command_buffer.cpp
@@ -60,6 +60,23 @@ namespace vkBasalt
         memoryBarrier.subresourceRange.baseArrayLayer = 0;
         memoryBarrier.subresourceRange.layerCount = 1;
         
+        //Reverses the first Barrier
+        VkImageMemoryBarrier secondBarrier;
+        secondBarrier.sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER;
+        secondBarrier.pNext = nullptr;
+        secondBarrier.srcAccessMask =  VK_ACCESS_SHADER_READ_BIT;
+        secondBarrier.dstAccessMask =  0;
+        secondBarrier.oldLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
+        secondBarrier.newLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
+        secondBarrier.srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+        secondBarrier.dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+        secondBarrier.image = VK_NULL_HANDLE;
+        secondBarrier.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+        secondBarrier.subresourceRange.baseMipLevel = 0;
+        secondBarrier.subresourceRange.levelCount = 1;
+        secondBarrier.subresourceRange.baseArrayLayer = 0;
+        secondBarrier.subresourceRange.layerCount = 1;
+        
         for(unsigned int i=0;i<count;i++)
         {
             std::cout << commandBuffers[i] << std::endl;
@@ -68,6 +85,7 @@ namespace vkBasalt
         for(unsigned int i=0;i<count;i++)
         {
             memoryBarrier.image = images[i];
+            secondBarrier.image = images[i];
             VkResult result = dispatchTable.BeginCommandBuffer(commandBuffers[i],&beginInfo);
             ASSERT_VULKAN(result);
             
@@ -94,6 +112,8 @@ namespace vkBasalt
             dispatchTable.CmdDraw(commandBuffers[i], 6, 1, 0, 0);
 
             dispatchTable.CmdEndRenderPass(commandBuffers[i]);
+            
+            dispatchTable.CmdPipelineBarrier(commandBuffers[i],VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT, VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT,0,0, nullptr,0, nullptr,1, &secondBarrier);
 
             result = dispatchTable.EndCommandBuffer(commandBuffers[i]);
             ASSERT_VULKAN(result);
