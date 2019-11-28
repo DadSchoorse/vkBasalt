6d2d0f256aa88f9765318e37f39be0f4ceb991e5
Georg Lehmann <dadschoorse@gmail.com>
2019-11-28 15:36:30 +0100

    rewrite cas using SimpleEffect

--- src/effect_cas.hpp
+++ src/effect_cas.hpp
@@ -12,38 +12,15 @@
 #include "vulkan/vk_layer.h"
 #include "vulkan/vk_layer_dispatch_table.h"
 
-#include "effect.hpp"
+#include "effect_simple.hpp"
 #include "config.hpp"
 
 namespace vkBasalt{
-    class CasEffect : public Effect
+    class CasEffect : public SimpleEffect
     {
     public:
         CasEffect(VkPhysicalDevice physicalDevice, VkLayerInstanceDispatchTable instanceDispatchTable, VkDevice device, VkLayerDispatchTable dispatchTable, VkFormat format,  VkExtent2D imageExtent, std::vector<VkImage> inputImages, std::vector<VkImage> outputImages, std::shared_ptr<vkBasalt::Config> pConfig);
-        void applyEffect(uint32_t imageIndex, VkCommandBuffer commandBuffer) override; 
         ~CasEffect();
-    private:
-        VkPhysicalDevice physicalDevice;
-        VkLayerInstanceDispatchTable instanceDispatchTable;
-        VkDevice device;
-        VkLayerDispatchTable dispatchTable;
-        std::vector<VkImage> inputImages;
-        std::vector<VkImage> outputImages;
-        std::vector<VkImageView> inputImageViews;
-        std::vector<VkImageView> outputImageViews;
-        std::vector<VkDescriptorSet> imageDescriptorSets;
-        std::vector<VkFramebuffer> framebuffers;
-        VkDescriptorSetLayout imageSamplerDescriptorSetLayout;
-        VkDescriptorPool descriptorPool;
-        VkShaderModule vertexModule;
-        VkShaderModule fragmentModule;
-        VkRenderPass renderPass;
-        VkPipelineLayout pipelineLayout;
-        VkPipeline graphicsPipeline;
-        VkExtent2D imageExtent;
-        VkFormat format;
-        VkSampler sampler;
-        std::shared_ptr<vkBasalt::Config> pConfig;
     };
 }
 
