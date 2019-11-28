591b69a218841fe66fb362db52a26d748e54bf77
Georg Lehmann <dadschoorse@gmail.com>
2019-11-28 16:13:08 +0100

    use options from config file for deband

--- src/effect_deband.cpp
+++ src/effect_deband.cpp
@@ -29,8 +29,60 @@ namespace vkBasalt
         shaderInfo.vertexCode = readFile(fullScreenRectFile.c_str());
         shaderInfo.fragmentCode = readFile(debandFragmentFile.c_str());
         
+        struct{
+            float     screenWidth;
+            float     screenHeight;
+            float     reverseScreenWidth;
+            float     reverseScreenHeight;
+            float     debandAvgdiff = 3.4;
+            float     debandMaxdiff = 6.8;
+            float     debandMiddiff = 3.3;
+            float     range = 16.0;
+            int32_t   iterations = 4;
+        } debandOptions;
+        
+        debandOptions.screenWidth = (float) imageExtent.width;
+        debandOptions.screenHeight = (float) imageExtent.height;
+        debandOptions.reverseScreenWidth = 1.0f / imageExtent.width;
+        debandOptions.reverseScreenHeight = 1.0f / imageExtent.height;
+        //get Options
+        if(pConfig->getOption("debandAvgdiff")!=std::string(""))
+        {
+            debandOptions.debandAvgdiff = std::stod(pConfig->getOption("debandAvgdiff"));
+        }
+        if(pConfig->getOption("debandMaxdiff")!=std::string(""))
+        {
+            debandOptions.debandMaxdiff = std::stod(pConfig->getOption("debandMaxdiff"));
+        }
+        if(pConfig->getOption("debandMiddiff")!=std::string(""))
+        {
+            debandOptions.debandMiddiff = std::stod(pConfig->getOption("debandMiddiff"));
+        }
+        if(pConfig->getOption("debandRange")!=std::string(""))
+        {
+            debandOptions.range = std::stod(pConfig->getOption("debandRange"));
+        }
+        if(pConfig->getOption("debandIterations")!=std::string(""))
+        {
+            debandOptions.iterations = std::stod(pConfig->getOption("debandIterations"));
+        }
+        
+        std::vector<VkSpecializationMapEntry> specMapEntrys(9);
+        for(uint32_t i=0;i<specMapEntrys.size();i++)
+        {
+            specMapEntrys[i].constantID = i;
+            specMapEntrys[i].offset = sizeof(float) * i;//TODO not clean to assume that sizeof(int32_t) == sizeof(float)
+            specMapEntrys[i].size = sizeof(float);
+        }
+        
+        VkSpecializationInfo specializationInfo;
+        specializationInfo.mapEntryCount = specMapEntrys.size();
+        specializationInfo.pMapEntries = specMapEntrys.data();
+        specializationInfo.dataSize = sizeof(debandOptions);
+        specializationInfo.pData = &debandOptions;
+        
         shaderInfo.pVertexSpecInfo = nullptr;
-        shaderInfo.pFragmentSpecInfo = nullptr;
+        shaderInfo.pFragmentSpecInfo = &specializationInfo;
         
         init(physicalDevice, instanceDispatchTable, device, dispatchTable, format,  imageExtent, inputImages, outputImages, pConfig);
     }
