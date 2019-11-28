eb49e3f097a4fc62031c1bc1de42d8e29e6f256f
Georg Lehmann <dadschoorse@gmail.com>
2019-11-28 15:30:37 +0100

    rewrite fxaa to use simple effect

--- src/effect_fxaa.cpp
+++ src/effect_fxaa.cpp
@@ -25,80 +25,24 @@ namespace vkBasalt
     {
         std::string fullScreenRectFile = std::string(getenv("HOME")) + "/.local/share/vkBasalt/shader/full_screen_triangle.vert.spv";
         std::string fxaaFragmentFile = std::string(getenv("HOME")) + "/.local/share/vkBasalt/shader/fxaa.frag.spv";
-        std::cout << "in creating FxaaEffect " << std::endl;
-        
-        this->physicalDevice = physicalDevice;
-        this->instanceDispatchTable = instanceDispatchTable;
-        this->device = device;
-        this->dispatchTable = dispatchTable;
-        this->format = format;
-        this->imageExtent = imageExtent;
-        this->inputImages = inputImages;
-        this->outputImages = outputImages;
-        this->pConfig = pConfig;
-        
-        inputImageViews = createImageViews(device, dispatchTable, format, inputImages);
-        std::cout << "after creating input ImageViews" << std::endl;
-        outputImageViews = createImageViews(device, dispatchTable, format, outputImages);
-        std::cout << "after creating ImageViews" << std::endl;
-        sampler = createSampler(device, dispatchTable);
-        std::cout << "after creating sampler" << std::endl;
-        
-        imageSamplerDescriptorSetLayout = createImageSamplerDescriptorSetLayout(device, dispatchTable, 1);
-        std::cout << "after creating descriptorSetLayouts" << std::endl;
-        
-        VkDescriptorPoolSize imagePoolSize;
-        imagePoolSize.type = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
-        imagePoolSize.descriptorCount = inputImages.size()+10;
-        
-        
-        std::vector<VkDescriptorPoolSize> poolSizes = {imagePoolSize};
-        
-        descriptorPool = createDescriptorPool(device, dispatchTable, poolSizes);
-        std::cout << "after creating descriptorPool" << std::endl;
-        
-        
-        
-        
-        float fxaaQualitySubpix;
-        float fxaaQualityEdgeThreshold;
-        float fxaaQualityEdgeThresholdMin;
+        float fxaaQualitySubpix = 0.75f;
+        float fxaaQualityEdgeThreshold = 0.125f;
+        float fxaaQualityEdgeThresholdMin = 0.0312f;
         if(pConfig->getOption("fxaaQualitySubpix")!=std::string(""))
         {
             fxaaQualitySubpix = std::stod(pConfig->getOption("fxaaQualitySubpix"));
-        }
-        else
-        {
-            fxaaQualitySubpix = 0.75f;
         }
         if(pConfig->getOption("fxaaQualityEdgeThreshold")!=std::string(""))
         {
             fxaaQualityEdgeThreshold = std::stod(pConfig->getOption("fxaaQualityEdgeThreshold"));
         }
-        else
-        {
-            fxaaQualityEdgeThreshold = 0.125f;
-        }
         if(pConfig->getOption("fxaaQualityEdgeThresholdMin")!=std::string(""))
         {
             fxaaQualityEdgeThresholdMin = std::stod(pConfig->getOption("fxaaQualityEdgeThresholdMin"));
         }
-        else
-        {
-            fxaaQualityEdgeThresholdMin = 0.0312f;
-        }
         
-        
-        auto vertexCode = readFile(fullScreenRectFile.c_str());
-        createShaderModule(device, dispatchTable, vertexCode, &vertexModule);
-    
-        auto fragmentCode = readFile(fxaaFragmentFile.c_str());
-        createShaderModule(device, dispatchTable, fragmentCode, &fragmentModule);
-        
-        renderPass = createRenderPass(device, dispatchTable, format);
-        
-        std::vector<VkDescriptorSetLayout> descriptorSetLayouts = {imageSamplerDescriptorSetLayout};
-        pipelineLayout = createGraphicsPipelineLayout(device, dispatchTable, descriptorSetLayouts);
+        shaderInfo.vertexCode   = readFile(fullScreenRectFile.c_str());
+        shaderInfo.fragmentCode = readFile(fxaaFragmentFile.c_str());
         
         std::vector<VkSpecializationMapEntry> specMapEntrys(5);
         
@@ -121,111 +65,13 @@ namespace vkBasalt
         fragmentSpecializationInfo.dataSize = sizeof(float)*specData.size();
         fragmentSpecializationInfo.pData = specData.data();
         
-        graphicsPipeline = createGraphicsPipeline(device, dispatchTable, vertexModule, nullptr, fragmentModule, &fragmentSpecializationInfo, imageExtent, renderPass, pipelineLayout);
+        shaderInfo.pVertexSpecInfo = nullptr;
+        shaderInfo.pFragmentSpecInfo = &fragmentSpecializationInfo;
         
-        
-        imageDescriptorSets = allocateAndWriteImageSamplerDescriptorSets(device,
-                                                                         dispatchTable,
-                                                                         descriptorPool,
-                                                                         imageSamplerDescriptorSetLayout,
-                                                                         sampler,
-                                                                         std::vector<std::vector<VkImageView>>(1,inputImageViews));
-        
-        framebuffers = createFramebuffers(device, dispatchTable, renderPass, imageExtent, outputImageViews);
-    }
-    void FxaaEffect::applyEffect(uint32_t imageIndex, VkCommandBuffer commandBuffer)
-    {
-        std::cout << "applying fxaa effect" << commandBuffer << std::endl;
-        //Used to make the Image accessable by the shader
-        VkImageMemoryBarrier memoryBarrier;
-        memoryBarrier.sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER;
-        memoryBarrier.pNext = nullptr;
-        memoryBarrier.srcAccessMask = VK_ACCESS_MEMORY_WRITE_BIT;
-        memoryBarrier.dstAccessMask = VK_ACCESS_SHADER_READ_BIT;
-        memoryBarrier.oldLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
-        memoryBarrier.newLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
-        memoryBarrier.srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
-        memoryBarrier.dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
-        memoryBarrier.image = inputImages[imageIndex];
-        memoryBarrier.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
-        memoryBarrier.subresourceRange.baseMipLevel = 0;
-        memoryBarrier.subresourceRange.levelCount = 1;
-        memoryBarrier.subresourceRange.baseArrayLayer = 0;
-        memoryBarrier.subresourceRange.layerCount = 1;
-        
-        //Reverses the first Barrier
-        VkImageMemoryBarrier secondBarrier;
-        secondBarrier.sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER;
-        secondBarrier.pNext = nullptr;
-        secondBarrier.srcAccessMask =  VK_ACCESS_SHADER_READ_BIT;
-        secondBarrier.dstAccessMask =  0;
-        secondBarrier.oldLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
-        secondBarrier.newLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
-        secondBarrier.srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
-        secondBarrier.dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
-        secondBarrier.image = inputImages[imageIndex];
-        secondBarrier.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
-        secondBarrier.subresourceRange.baseMipLevel = 0;
-        secondBarrier.subresourceRange.levelCount = 1;
-        secondBarrier.subresourceRange.baseArrayLayer = 0;
-        secondBarrier.subresourceRange.layerCount = 1;
-        
-        dispatchTable.CmdPipelineBarrier(commandBuffer,VK_PIPELINE_STAGE_BOTTOM_OF_PIPE_BIT, VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT,0,0, nullptr,0, nullptr,1, &memoryBarrier);
-        std::cout << "after the first pipeline barrier" << std::endl;
-        
-        std::cout << "framebuffer " << framebuffers.size() << std::endl;
-        
-        std::cout << "framebuffer " << framebuffers[imageIndex] << std::endl;
-
-        VkRenderPassBeginInfo renderPassBeginInfo;
-        renderPassBeginInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
-        renderPassBeginInfo.pNext = nullptr;
-        renderPassBeginInfo.renderPass = renderPass;
-        renderPassBeginInfo.framebuffer = framebuffers[imageIndex];
-        renderPassBeginInfo.renderArea.offset = {0,0};
-        renderPassBeginInfo.renderArea.extent = imageExtent;
-        VkClearValue clearValue = {0.0f, 0.0f, 0.0f, 1.0f};
-        renderPassBeginInfo.clearValueCount = 1;
-        renderPassBeginInfo.pClearValues = &clearValue;
-        
-        std::cout << "before beginn renderpass" << std::endl;
-        dispatchTable.CmdBeginRenderPass(commandBuffer,&renderPassBeginInfo,VK_SUBPASS_CONTENTS_INLINE);
-        std::cout << "after beginn renderpass" << std::endl;
-        
-        dispatchTable.CmdBindDescriptorSets(commandBuffer,VK_PIPELINE_BIND_POINT_GRAPHICS,pipelineLayout,0,1,&(imageDescriptorSets[imageIndex]),0,nullptr);
-        std::cout << "after binding image sampler" << std::endl;
-        
-        dispatchTable.CmdBindPipeline(commandBuffer,VK_PIPELINE_BIND_POINT_GRAPHICS,graphicsPipeline);
-        std::cout << "after bind pipeliene" << std::endl;
-        
-        dispatchTable.CmdDraw(commandBuffer, 3, 1, 0, 0);
-        std::cout << "after draw" << std::endl;
-
-        dispatchTable.CmdEndRenderPass(commandBuffer);
-        std::cout << "after end renderpass" << std::endl;
-        
-        dispatchTable.CmdPipelineBarrier(commandBuffer,VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT, VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT,0,0, nullptr,0, nullptr,1, &secondBarrier);
-        std::cout << "after the second pipeline barrier" << std::endl;
-
+        init(physicalDevice, instanceDispatchTable, device, dispatchTable, format,  imageExtent, inputImages, outputImages, pConfig);
     }
     FxaaEffect::~FxaaEffect()
     {
-        std::cout << "destroying fxaa effect " << this << std::endl;
-        dispatchTable.DestroyPipeline(device, graphicsPipeline, nullptr);
-        dispatchTable.DestroyPipelineLayout(device,pipelineLayout,nullptr);
-        dispatchTable.DestroyRenderPass(device,renderPass,nullptr);
-        dispatchTable.DestroyDescriptorSetLayout(device,imageSamplerDescriptorSetLayout,nullptr);
-        dispatchTable.DestroyShaderModule(device,vertexModule,nullptr);
-        dispatchTable.DestroyShaderModule(device,fragmentModule,nullptr);
-        
-        dispatchTable.DestroyDescriptorPool(device,descriptorPool,nullptr);
-        for(unsigned int i=0;i<framebuffers.size();i++)
-        {
-            dispatchTable.DestroyFramebuffer(device,framebuffers[i],nullptr);
-            dispatchTable.DestroyImageView(device,inputImageViews[i],nullptr);
-            dispatchTable.DestroyImageView(device,outputImageViews[i],nullptr);
-            std::cout << "after DestroyImageView" << std::endl;
-        }
-        dispatchTable.DestroySampler(device,sampler,nullptr);
+    
     }
 }
