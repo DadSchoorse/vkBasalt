51fda734a4fd373799c1968fe366a8e4cac4eadc
Georg Lehmann <dadschoorse@gmail.com>
2019-12-05 12:38:30 +0100

    added lut shader

---
+++ shader/lut.frag.glsl
@@ -0,0 +1,21 @@
+#version 450
+
+layout(set=0, binding=0) uniform sampler2D img;
+layout(set=1, binding=0) uniform sampler3D lut;
+
+//Only works with cubes not with cuboids
+layout(constant_id = 0) const int lutSize = 32;
+
+layout(location = 0) in vec2 textureCoord;
+layout(location = 0) out vec4 fragColor;
+
+void main()
+{
+    vec4 color = texture(img,textureCoord);
+    
+    //see https://developer.nvidia.com/gpugems/GPUGems2/gpugems2_chapter24.html
+    vec3 scale = (vec3(lutSize) - 1.0) / vec3(lutSize);
+    vec3 offset = 1.0 / (2.0 * vec3(lutSize));
+    
+    fragColor = vec4(texture(lut, scale * color.rgb + offset).rgb, color.a);
+}
