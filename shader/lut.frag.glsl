c934b0805c2c5eba46fde1adc0049d3b2603309e
Georg Lehmann <dadschoorse@gmail.com>
2019-12-06 15:22:04 +0100

    flip to rbga in lut if using png

--- shader/lut.frag.glsl
+++ shader/lut.frag.glsl
@@ -5,13 +5,22 @@ layout(set=1, binding=0) uniform sampler3D lut;
 
 //Only works with cubes not with cuboids
 layout(constant_id = 0) const int lutSize = 32;
+layout(constant_id = 1) const int flipGB = 0;
 
 layout(location = 0) in vec2 textureCoord;
 layout(location = 0) out vec4 fragColor;
 
 void main()
 {
-    vec4 color = texture(img,textureCoord);
+    vec4 color;
+    if(flipGB != 0)
+    {
+        color = texture(img,textureCoord).rbga;
+    }
+    else
+    {
+        color = texture(img,textureCoord);
+    }
     
     //see https://developer.nvidia.com/gpugems/GPUGems2/gpugems2_chapter24.html
     vec3 scale = (vec3(lutSize) - 1.0) / vec3(lutSize);
