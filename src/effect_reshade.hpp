e4def207aa8581d74365a5e3a21b5f4c572846f5
Georg Lehmann <dadschoorse@gmail.com>
2019-12-13 19:10:35 +0100

    add reshade effect

---
+++ src/effect_reshade.hpp
@@ -0,0 +1,57 @@
+#ifndef EFFECT_RESHADE_HPP_INCLUDED
+#define EFFECT_RESHADE_HPP_INCLUDED
+#include <vector>
+#include <fstream>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <memory>
+
+#include "vulkan/vulkan.h"
+#include "vulkan/vk_layer.h"
+#include "vulkan/vk_layer_dispatch_table.h"
+
+#include "effect.hpp"
+#include "config.hpp"
+
+#include "../reshade/source/effect_parser.hpp"
+#include "../reshade/source/effect_codegen.hpp"
+#include "../reshade/source/effect_preprocessor.hpp"
+
+namespace vkBasalt{
+    class ReshadeEffect : public Effect
+    {
+    public:
+        ReshadeEffect(VkPhysicalDevice physicalDevice, VkLayerInstanceDispatchTable instanceDispatchTable, VkDevice device, VkLayerDispatchTable dispatchTable, VkFormat format,  VkExtent2D imageExtent, std::vector<VkImage> inputImages, std::vector<VkImage> outputImages, std::shared_ptr<vkBasalt::Config> pConfig, VkQueue queue, VkCommandPool commandPool);
+        void virtual applyEffect(uint32_t imageIndex, VkCommandBuffer commandBuffer) override;
+        virtual ~ReshadeEffect();
+    private:
+        VkPhysicalDevice physicalDevice;
+        VkLayerInstanceDispatchTable instanceDispatchTable;
+        VkDevice device;
+        VkLayerDispatchTable dispatchTable;
+        std::vector<VkImage> inputImages;
+        std::vector<VkImage> outputImages;
+        std::vector<VkImageView> inputImageViews;
+        std::vector<VkImageView> outputImageViews;
+        std::vector<VkDescriptorSet> imageDescriptorSets;
+        std::vector<VkFramebuffer> framebuffers;
+        VkDescriptorSetLayout imageSamplerDescriptorSetLayout;
+        VkDescriptorPool descriptorPool;
+        VkShaderModule shaderModule;
+        VkShaderModule fragmentModule;
+        VkRenderPass renderPass;
+        VkPipelineLayout pipelineLayout;
+        VkPipeline graphicsPipeline;
+        VkExtent2D imageExtent;
+        VkFormat format;
+        VkSampler sampler;
+        std::shared_ptr<vkBasalt::Config> pConfig;
+        
+        void createReshadeModule(reshadefx::module& module);
+    };
+}
+
+
+#endif // EFFECT_RESHADE_HPP_INCLUDED
