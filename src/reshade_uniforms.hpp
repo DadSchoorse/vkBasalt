d3f3e084dfc9b1e31bbe3c45a8dca58d35505873
Georg Lehmann <dadschoorse@gmail.com>
2020-01-12 14:55:45 +0100

    [reshade] add inital uniform classes

---
+++ src/reshade_uniforms.hpp
@@ -0,0 +1,136 @@
+#ifndef RESHADE_UNIFORMS_HPP_INCLUDED
+#define RESHADE_UNIFORMS_HPP_INCLUDED
+#include <vector>
+#include <fstream>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <chrono>
+
+#include "vulkan/vulkan.h"
+#include "vulkan/vk_layer.h"
+#include "vulkan/vk_layer_dispatch_table.h"
+
+#include "../reshade/source/effect_module.hpp"
+
+namespace vkBasalt
+{
+    void enumerateUniforms(reshadefx::module module);
+    
+    class ReshadeUniform
+    {
+    public:
+        void virtual update(void* mapedBuffer) = 0;
+        virtual ~ReshadeUniform(){};
+    protected:
+        uint32_t offset;
+        uint32_t size;
+    };
+    
+    class FrameTimeUniform : public ReshadeUniform
+    {
+    public:
+        FrameTimeUniform(reshadefx::uniform_info uniformInfo);
+        void virtual update(void* mapedBuffer) override;
+        virtual ~FrameTimeUniform();
+    private:
+        std::chrono::time_point<std::chrono::high_resolution_clock> lastFrame;
+    };
+    
+    class FrameCountUniform : public ReshadeUniform
+    {
+    public:
+        FrameCountUniform(reshadefx::uniform_info uniformInfo);
+        void virtual update(void* mapedBuffer) override;
+        virtual ~FrameCountUniform();
+    private:
+        int32_t count = 0;
+    };
+    
+    class DateUniform : public ReshadeUniform
+    {
+    public:
+        DateUniform(reshadefx::uniform_info uniformInfo);
+        void virtual update(void* mapedBuffer) override;
+        virtual ~DateUniform();
+    };
+    
+    class TimerUniform : public ReshadeUniform
+    {
+    public:
+        TimerUniform(reshadefx::uniform_info uniformInfo);
+        void virtual update(void* mapedBuffer) override;
+        virtual ~TimerUniform();
+    private:
+        std::chrono::time_point<std::chrono::high_resolution_clock> start;
+    };
+    
+    class PingPongUniform : public ReshadeUniform
+    {
+    public:
+        PingPongUniform(reshadefx::uniform_info uniformInfo);
+        void virtual update(void* mapedBuffer) override;
+        virtual ~PingPongUniform();
+    private:
+        std::chrono::time_point<std::chrono::high_resolution_clock> lastFrame;
+        float min  = 0.0f;
+        float max  = 0.0f;
+        float stepMin = 0.0f;
+        float stepMax = 0.0f;
+        float smoothing = 0.0f;
+        float currentValue[2] = {0.0f, 1.0f};
+    };
+    
+    class RandomUniform : public ReshadeUniform
+    {
+    public:
+        RandomUniform(reshadefx::uniform_info uniformInfo);
+        void virtual update(void* mapedBuffer) override;
+        virtual ~RandomUniform();
+    private:
+        int max = 0;
+        int min = 0;
+    };
+    
+    class KeyUniform : public ReshadeUniform
+    {
+    public:
+        KeyUniform(reshadefx::uniform_info uniformInfo);
+        void virtual update(void* mapedBuffer) override;
+        virtual ~KeyUniform();
+    };
+    
+    class MouseButtonUniform : public ReshadeUniform
+    {
+    public:
+        MouseButtonUniform(reshadefx::uniform_info uniformInfo);
+        void virtual update(void* mapedBuffer) override;
+        virtual ~MouseButtonUniform();
+    };
+    
+    class MousePointUniform : public ReshadeUniform
+    {
+    public:
+        MousePointUniform(reshadefx::uniform_info uniformInfo);
+        void virtual update(void* mapedBuffer) override;
+        virtual ~MousePointUniform();
+    };
+    
+    class MouseDeltaUniform : public ReshadeUniform
+    {
+    public:
+        MouseDeltaUniform(reshadefx::uniform_info uniformInfo);
+        void virtual update(void* mapedBuffer) override;
+        virtual ~MouseDeltaUniform();
+    };
+    
+    class DepthUniform : public ReshadeUniform
+    {
+    public:
+        DepthUniform(reshadefx::uniform_info uniformInfo);
+        void virtual update(void* mapedBuffer) override;
+        virtual ~DepthUniform();
+    };
+}
+
+#endif // RESHADE_UNIFORMS_HPP_INCLUDED
