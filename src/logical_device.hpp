0a33386f292f4b171b4497687e095fdaf0b69ac9
Georg Lehmann <dadschoorse@gmail.com>
2020-01-17 17:52:52 +0100

    rewritten layer base

---
+++ src/logical_device.hpp
@@ -0,0 +1,27 @@
+#ifndef LOGICAL_DEVICE_HPP_INCLUDED
+#define LOGICAL_DEVICE_HPP_INCLUDED
+#include <vector>
+#include <fstream>
+#include <string>
+#include <iostream>
+#include <vector>
+
+#include "vulkan_include.hpp"
+
+namespace vkBasalt
+{
+    struct LogicalDevice
+    {
+        VkLayerDispatchTable         vkd;
+        VkLayerInstanceDispatchTable vki;
+        VkDevice                     device;
+        VkPhysicalDevice             physicalDevice;
+        VkInstance                   instance;
+        VkQueue                      queue;
+        uint32_t                     queueFamilyIndex;
+        VkCommandPool                commandPool;
+    };
+}
+
+
+#endif // LOGICAL_DEVICE_HPP_INCLUDED
