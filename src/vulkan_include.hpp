ae0b9f5159c5df06d2fd2ab30d67292b41b3afe8
Georg Lehmann <dadschoorse@gmail.com>
2020-02-10 15:28:38 +0100

    improve ASSERT_VULKAN

--- src/vulkan_include.hpp
+++ src/vulkan_include.hpp
@@ -10,11 +10,13 @@
 #include "vulkan/vk_layer_dispatch_table.h"
 #include "vulkan/vk_dispatch_table_helper.h"
 
+#include <string>
+
 #ifndef ASSERT_VULKAN
 #define ASSERT_VULKAN(val)\
         if(val!=VK_SUCCESS)\
         {\
-            throw std::runtime_error("ASSERT_VULKAN failed " + std::to_string(val));\
+            throw std::runtime_error("ASSERT_VULKAN failed in " + std::string(__FILE__) + " : " + std::to_string(__LINE__) + "; " + std::to_string(val));\
         }
 #endif
 
