db15e964c8559de6b73cdd535b38b72c8e2f7619
Georg Lehmann <dadschoorse@gmail.com>
2020-05-03 13:22:34 +0200

    rework full screen triangle

--- shader/full_screen_triangle.vert.glsl
+++ shader/full_screen_triangle.vert.glsl
@@ -2,21 +2,13 @@
 
 layout(location = 0) out vec2 textureCoord;
 
-vec2 positions[3] = vec2[](
-    vec2(-1.0,-1.0),
-    vec2( 3.0,-1.0),
-    vec2(-1.0, 3.0)
-);
-
-vec2 texture_coordinates[3] = vec2[](
-    vec2( 0.0, 0.0),
-    vec2( 2.0, 0.0),
-    vec2( 0.0, 2.0)
-);
-
-
 void main() {
-    gl_Position = vec4(positions[gl_VertexIndex], 0.0, 1.0);
-    textureCoord = texture_coordinates[gl_VertexIndex];
+    gl_Position = vec4(gl_VertexIndex == 1 ? 3.0 : -1.0, 
+                       gl_VertexIndex == 2 ? 3.0 : -1.0,
+                       0.0,
+                       1.0);
+
+    textureCoord = vec2(gl_VertexIndex == 1 ? 2.0 : 0.0, 
+                        gl_VertexIndex == 2 ? 2.0 : 0.0);
 }
 
