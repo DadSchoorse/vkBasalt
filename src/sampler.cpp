77ec88d41773273bb7e67beece1e686cca4c5188
Georg Lehmann <dadschoorse@gmail.com>
2020-12-31 14:33:58 +0100

    use CLAMP_TO_EDGE for samplers instead of REPEAT

--- src/sampler.cpp
+++ src/sampler.cpp
@@ -13,9 +13,9 @@ namespace vkBasalt
         samplerCreateInfo.magFilter               = VK_FILTER_LINEAR;
         samplerCreateInfo.minFilter               = VK_FILTER_LINEAR;
         samplerCreateInfo.mipmapMode              = VK_SAMPLER_MIPMAP_MODE_LINEAR;
-        samplerCreateInfo.addressModeU            = VK_SAMPLER_ADDRESS_MODE_REPEAT;
-        samplerCreateInfo.addressModeV            = VK_SAMPLER_ADDRESS_MODE_REPEAT;
-        samplerCreateInfo.addressModeW            = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+        samplerCreateInfo.addressModeU            = VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE;
+        samplerCreateInfo.addressModeV            = VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE;
+        samplerCreateInfo.addressModeW            = VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE;
         samplerCreateInfo.mipLodBias              = 0.0f;
         samplerCreateInfo.anisotropyEnable        = VK_FALSE;
         samplerCreateInfo.maxAnisotropy           = 16;
